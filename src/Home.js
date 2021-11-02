7e641bbdd46cfd469819358071285f16e65714c2
vishnu2193 <vvishnu760@gmail.com>
2021-11-02 08:09:50 +0530

    1 file modified, 2 added, 1 deleted

---
+++ src/Home.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import axios from 'axios';
+import Paper from '@mui/material/Paper';
+import AccountMenu from './AccountMenu';
+import Tabb from './Tabb';
+
+const Router = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchnews = () => {
+      // Update the document title using the browser API
+      axios({
+        url: `https://jsonplaceholder.typicode.com/posts`,
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then((responce) => {
+          setPosts(responce.data);
+        })
+        .catch();
+    };
+    fetchnews();
+  }, []);
+
+  return (
+    <>
+      <Paper square elevation={5} style={{ display: 'flex', width: '1000%' }}>
+        <img
+          style={{ width: '190px', height: '80px' }}
+          src="https://www.vhv.rs/dpng/d/134-1343401_movie-film-strip-clipart-transparent-background-film-logo.png"
+        />
+        <h2>Movies</h2>
+        <AccountMenu style={{}} />
+      </Paper>
+      <Tabb />
+      {posts.map((item, i) => {
+        return <div key={i}></div>;
+      })}
+    </>
+  );
+};
+export default Router;
---
+++ src/Router.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+import Home from './Home';
+
+const Router = () => {
+  return <Home />;
+};
+export default Router;
