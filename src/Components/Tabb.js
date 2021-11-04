e3fdad08bbd4ca133f64cb15b73474c9e4fb549d
vishnu2193 <vvishnu760@gmail.com>
2021-11-04 15:02:50 +0530

    1 file modified

--- src/Components/Tabb.js
+++ src/Components/Tabb.js
@@ -95,36 +95,30 @@ export default function BasicTabs() {
       <TabPanel value={value} index={1}>
         {posts.map((item, i) => {
           return (
-            <Paper
-              square
-              elevation={5}
-              style={{
-                display: 'inline-flex',
-                width: '150px',
-                height: '200px',
-                margin: '25px',
-              }}
-            >
-              <img src="https://cdn.shopclues.com/images1/thumbnails/110871/280/1/151047473-110871442-1601973238.jpg" />
-            </Paper>
+            <div class="Imageboxs">
+              <img
+                style={{ width: '250px', height: '250px' }}
+                src="https://5.imimg.com/data5/ANDROID/Default/2020/10/DA/GX/TA/115146420/product-jpeg-500x500.jpg"
+                alt=""
+              />
+              <p>Beware of the leopard dsfdsfd dfgdfd</p>
+              <p>Rs 200</p>
+            </div>
           );
         })}
       </TabPanel>
       <TabPanel value={value} index={2}>
         {posts.map((item, i) => {
           return (
-            <Paper
-              square
-              elevation={5}
-              style={{
-                display: 'inline-flex',
-                width: '150px',
-                height: '200px',
-                margin: '25px',
-              }}
-            >
-              <img src="https://medias.utsavfashion.com/media/catalog/product/cache/1/small_image/295x/040ec09b1e35df139433887a97daa66f/d/i/digital-printed-art-silk-jacket-style-gown-in-blue-and-pink-v1-uku1075.jpg" />
-            </Paper>
+            <div class="Imageboxs">
+              <img
+                style={{ width: '250px', height: '250px' }}
+                src="https://5.imimg.com/data5/ANDROID/Default/2020/10/DA/GX/TA/115146420/product-jpeg-500x500.jpg"
+                alt=""
+              />
+              <p>Beware of the leopard dsfdsfd dfgdfd</p>
+              <p>Rs 200</p>
+            </div>
           );
         })}
       </TabPanel>
